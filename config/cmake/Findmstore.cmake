77a242d874ecbd5c3994db2bd65b690147043012
Daniel Mejia-Rodriguez <daniel.mejia@pnnl.gov>
2022-04-05 03:38:53 -0400

    Set tags for mstore and mctc

--- config/cmake/Findmstore.cmake
+++ config/cmake/Findmstore.cmake
@@ -16,6 +16,7 @@
 set(_lib "mstore")
 set(_pkg "MSTORE")
 set(_url "https://github.com/grimme-lab/mstore")
+set(_tag "v0.2.0")
 
 if(NOT DEFINED "${_pkg}_FIND_METHOD")
   if(DEFINED "${PROJECT_NAME}-dependency-method")
@@ -28,7 +29,7 @@ endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/multicharge-utils.cmake")
 
-multicharge_find_package("${_lib}" "${${_pkg}_FIND_METHOD}" "${_url}")
+multicharge_find_package("${_lib}" "${${_pkg}_FIND_METHOD}" "${_url}" "${_tag}")
 
 if(DEFINED "_${_pkg}_FIND_METHOD")
   unset("${_pkg}_FIND_METHOD")
@@ -37,3 +38,4 @@ endif()
 unset(_lib)
 unset(_pkg)
 unset(_url)
+unset(_tag)
